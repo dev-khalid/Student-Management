17abf18cfbc50a4494d00cd3c58b48fe627c4ad5
dev-khalid <khalid.hossain.akash@gmail.com>
2022-02-10 10:27:30 +0600

    heroku test

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 import express from 'express'; 
 import dotenv from 'dotenv'; 
+import path from 'path'; 
 dotenv.config(); 
+const __dirname = path.resolve();
 const app = express();
 
 app.get('/api',(req,res,next) => { 
   res.send("Hello from Backend"); 
-}) 
+})  
 
+
+
+
+if (process.env.NODE_ENV == 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+  });
+}
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT,() => console.log(`Server is running on port ${PORT}`))
